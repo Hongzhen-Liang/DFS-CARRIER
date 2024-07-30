f91e34fa48726cb73110f011d141ea3af720634b
rampo.liang <rampo.liang@vipshop.com>
2024-07-30 15:13:04 +0800

    finish partial simple redis

---
+++ src/main/java/cn/sinscry/centralize/KeyValue/ICache/Main.java
@@ -0,0 +1,19 @@
+package cn.sinscry.centralize.KeyValue.ICache;
+
+import java.util.concurrent.TimeUnit;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        ICache<String, String> cache = new ICache<>();
+        cache.put("1", "1");
+        cache.put("2", "2");
+
+        cache.expire("1", 100);
+        assert cache.size()==2;
+        System.out.println("Before expire: " + cache.keySet());
+
+        TimeUnit.MILLISECONDS.sleep(500);
+        assert cache.size()==1;
+        System.out.println("After expire: " + cache.keySet());
+    }
+}
